ed38776793574ef79c59d957853c9afc599502b9
E_Davron <ddavronjon123@gmail.com>
2022-09-08 06:34:34 -0700

    responsive design

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,28 @@
 <template>
-        <nav class="flex justify-between items-center bg-gray-200 p-4 lg:py-6 lg:px-14 shadow-lg mb-4">
+        <nav class="flex justify-between items-center bg-gray-200 p-4 lg:py-6 lg:px-14 shadow-lg mb-4 relative">
             <div class="flex items-center space-x-2">
                 <img class="h-7 lg:h-8" src="../assets/logo.png" alt="Alt">
-                <h1 class="text-xl text-pink-700 font-serif font-bold lg:text-2xl">{{ brand }}</h1>
+                <h1 class="text-xl text-pink-700 font-serif font-bold lg:text-2xl">{{ $store.state.brand }}</h1>
             </div>
-            <div>
-                <ul class="flex space-x-3 lg:space-x-10 lg:ml-6">
+            <!-- mobile menu -->
+            <div class="lg:hidden">
+                <button @click="openMenu()" class="fa-solid fa-bars absolute right-6 top-2 text-pink-700 font-semibold text-xl active:bg-gray-300 rounded-lg px-1 py-2">
+                </button>
+                <div v-if="result" class="relative">
+                    <ul class="lg:flex lg:space-x-10 lg:ml-6 absolute flex flex-col top-2 px-4 py-3 right-7 w-90 h-90 bg-gray-300 shadow-lg rounded-lg">
+                        <router-link v-for="yollanma in yollanmalar" :key="yollanma" :to="yollanma.manzil" class=" text-pink-700 text-md uppercase font-bold font-sans lg:font-bold lg:tracking-wider lg:text-[19px]">{{ yollanma.nom }}</router-link>
+                    </ul>
+                </div>
+            </div>
+            <!-- /mobile menu -->
+
+            <!-- desktop menu -->
+            <div class="hidden lg:inline-block">
+                <ul class="lg:flex lg:space-x-10 lg:ml-6">
                     <router-link v-for="yollanma in yollanmalar" :key="yollanma" :to="yollanma.manzil" class=" text-pink-700 text-md uppercase font-semibold font-sans lg:font-bold lg:tracking-wider lg:text-[19px]">{{ yollanma.nom }}</router-link>
                 </ul>
             </div>
+            <!-- /desktop menu -->
             <div class="hidden lg:flex items-center space-x-3">
                 <input v-model="title" class="py-1 rounded-lg tracking-widest font-semibold font-sans border outline-none border-amber-400 pl-2" type="text" placeholder="Enter Your Name">
                 <button @click="changeBrand()" class="fa-sharp fa-solid fa-magnifying-glass text-xl text-amber-700"></button>
@@ -31,18 +45,21 @@ export default {
                     manzil: '/about',
                     nom: 'About'
                 },                
-            ],            
-            title: '',
-            brand: 'Portfolio',
+            ],
+            title: '',            
+            result: false,
         }
     },
     methods: {
         changeBrand() {
-            if(this.brand == "") {
-                alert('Yozish esingdami botalogim');
+            if(this.store.state.brand == "") {
+                alert('Enter Your brand');
             } else {
-                this.brand = this.title;
+                this.$store.state.brand = this.title;
             }
+        },
+        openMenu() {
+            this.result = !this.result;
         }
     }
 }
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    brand: 'Prortfolio'
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
